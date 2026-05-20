02906d3ab8c3e5d7d248edb99e53a6004046ca07
areidakpena-ssis <153045302+areidakpena-ssis@users.noreply.github.com>
2026-05-20 15:08:09 +0700

    Update picture.py - added yellow triangles back in
    Accidentally removed yellow on prior merge.

--- picture.py
+++ picture.py
@@ -27,16 +27,18 @@ def draw_picture(width, height):
         
     
     # code for BLUE triangles  #Jay
-     sg.set_fill_color(colors[2]) # set fill color to red
+    sg.set_fill_color(colors[2]) # set fill color to red
     
     for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
         sg.fill_triangle(2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT,2*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT)
     
     # code for YELLOW triangles
+    sg.set_fill_color(colors[5])
+    for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
+        sg.fill_triangle(1*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 2*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
    
-   
-    # code for GREEN triangles
-  # code for GREEN triangles - Crystal
+
+     # code for GREEN triangles - Crystal
     sg.set_fill_color("#00ff00") # set fill color to red
     
     for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
